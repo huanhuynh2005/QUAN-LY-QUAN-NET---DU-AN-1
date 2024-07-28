5ec99f62c1f3b97fc18669f62abe92082c65d718
Huan Huynh <151844146+HuanHuynh161021@users.noreply.github.com>
2024-07-28 16:15:58 +0700

    [C09] CRUD Khách Hàng

---
+++ Models/VaiTro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace UIDuAn1.Models
+{
+    public partial class VaiTro
+    {
+        public VaiTro()
+        {
+            NhanVien = new HashSet<NhanVien>();
+        }
+
+        public string MaVaiTro { get; set; }
+        public string TenVaiTro { get; set; }
+
+        public virtual ICollection<NhanVien> NhanVien { get; set; }
+    }
+}
